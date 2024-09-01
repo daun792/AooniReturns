3fa5db137fbaa432cb0e9f1b59d06f7cb4772140
dah <daun792@gmail.com>
2024-09-01 20:53:36 +0900

    [Add]: TitleScene UI 배치

--- Assets/02. Script/TitleManager.cs
+++ Assets/02. Script/TitleManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button cafeBtn;
+    [SerializeField] Button loginBtn;
+    [SerializeField] Button signUpBtn;
+    [SerializeField] Button cancelBtn;
+    [SerializeField] GameObject signUpBack;
+
+    private void Start()
     {
-        
+        App.Manager.Sound.PlayBGM("BGM_Title");
+
+        cafeBtn.onClick.AddListener(OnClickCafe);
+        loginBtn.onClick.AddListener(OnClickCafe);
+        signUpBtn.onClick.AddListener(OnClickSignUp);
+        cancelBtn.onClick.AddListener(OnClickCancel);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClickCafe()
     {
-        
+        Application.OpenURL("https://m.cafe.naver.com/onireturns");
+    }
+
+    private void OnClickLogin()
+    {
+        App.LoadScene(SceneName.Lobby);
+    }
+
+    private void OnClickSignUp()
+    {
+        signUpBack.SetActive(true);
+    }
+
+    private void OnClickCancel()
+    {
+        signUpBack.SetActive(false);
     }
 }
